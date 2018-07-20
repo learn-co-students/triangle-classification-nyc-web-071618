71901d598c3db27a9c44001543db5d9f09ef23fb
chejianchao <820992376@qq.com>
2018-07-20 22:43:44 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+class TriangleError < StandardError
+  
+end
 class Triangle
   # write code here
-end
+  TriangleError
+  def initialize(a,b,c)
+    if a <=0 || b <= 0 || c<=0 
+      raise TriangleError
+    end
+    if a + b <= c || a+c <=b || b+c <= a 
+      raise TriangleError
+    end
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    if @a== @b && @b == @c 
+      :equilateral
+    elsif ( @a == @b || @b == @c || @a == @c)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+end # end triangle class
